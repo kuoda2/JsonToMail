46f3141bc567aee28059657debb0f94c8edfaa7b
Eddie <eddie_kuo@systemweb.com.tw>
2017-07-26 16:08:55 +0800

    temp

--- JsonToMail/JsonHelper.cs
+++ JsonToMail/JsonHelper.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.IO;
-using System.Text.RegularExpressions;
-using System.Data;
 
 namespace JsonToMail
 {
